0c4392ad292bb364c557c4156ffa6447ada329c8
OlivMagno <olivmagno1@gmail.com>
2022-07-20 20:33:21 -0300

    fix for main address path

--- src/index/components/menu/MenuNav.vue
+++ src/index/components/menu/MenuNav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/index/">About me</router-link>
+    <router-link to="/">Sobre</router-link>
     <router-link to="/index/folio">Portfolio</router-link>
     <router-link to="/index/skills">Conhecimentos</router-link>
   </nav>
