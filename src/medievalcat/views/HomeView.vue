2b3e117478e62f2f3765451ad33de2ceffd1c051
OlivMagno <olivmagno1@gmail.com>
2022-08-17 14:07:29 -0300

    added medievalcat SPA content

---
+++ src/medievalcat/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-image"></div>
+  <div class="home">
+    <MenuStructure />
+    <BodyHeader />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MenuStructure from "@/components/menu/MenuStructure.vue";
+import BodyHeader from "@/components/body/BodyHeader.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    MenuStructure,
+    BodyHeader,
+  },
+};
+</script>
+
+<style scoped>
+.bg-image {
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.52),
+      rgba(0, 0, 0, 1)
+    ),
+    url("@/assets/images/tempero-bg.jpg");
+  filter: blur(4px);
+  -webkit-filter: blur(4px);
+  background-size: 100vw;
+  height: 100vh;
+}
+</style>
